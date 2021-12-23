f824806d8c851731223e5b03bb5ab0d82b3985a3
h8c2 <37017782+h8c2@users.noreply.github.com>
2021-12-23 16:59:34 +0800

    Update config1.py

--- configs/config1.py
+++ configs/config1.py
@@ -54,7 +54,7 @@ args = {
     'weight_decay':1e-4,
     'lr': 0.05,
     'batch_size': 16,
-    'max_epochs': 10,
+    'max_epochs': 4,
     'scheduler': {"method":"cosine","warmup_epochs": 1},
     
 
