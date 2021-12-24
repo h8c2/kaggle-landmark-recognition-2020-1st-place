4978c547694b741b7f6df65ba453e48d85234d5b
h8c2 <37017782+h8c2@users.noreply.github.com>
2021-12-24 22:05:24 +0800

    Update config6.py

--- configs/config6.py
+++ configs/config6.py
@@ -6,21 +6,25 @@ abs_path = os.path.dirname(__file__)
 # os.environ["CUDA_VISIBLE_DEVICES"]= "0"
 
 args = {
-    'model_path':'../models/',
-    'data_path':'/ssd/kaggle_landmark/input/',
-    'data_path_2019':'/ssd/kaggle_landmark/2019/',
-    'valid_csv_fn':'recognition_solution_v2.1.csv',
+   'model_path':'../models/',
+    'data_path':'/kaggle/input/landmark-recognition-2021/',
+    'data_path_valid':'/kaggle/input/google-landmark-2021-validation/',
+    'valid_csv_fn':'valid.csv',
     'train_csv_fn':'train.csv',
-
-    'gpus':'0,1',
+    'checkpoint_path':'',
+    
+    'train_slice':[370000,450000],
+    'valid_slice':[35000,42000],
+    
+    'gpus':1,
     'filter_warnings':True,
     'logger': 'neptune',
     'num_sanity_val_steps': 50,
 
-    'distributed_backend': 'ddp',
+    'distributed_backend': '',
 
-    'gradient_accumulation_steps':3,
-    'precision':16,
+    'gradient_accumulation_steps':1,
+    'precision':32,
     'sync_batchnorm':False,
     
     'seed':4242,
@@ -51,8 +55,8 @@ args = {
     'optimizer': "sgd",
     'weight_decay':1e-4,
     'lr': 0.05,
-    'batch_size': 40,
-    'max_epochs': 10,
+    'batch_size': 14,
+    'max_epochs': 4,
     'scheduler': {"method":"cosine","warmup_epochs": 1},
     
 
